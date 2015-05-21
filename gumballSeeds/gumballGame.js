b6dc9b78a36533cc6931a3e420eb94c866668f91
Alexander Tappin <alextappin@gmail.com>
2015-05-21 06:26:27 +0000

    CreatedRunningAnimationandcharacterpicture

--- gumballSeeds/gumballGame.js
+++ gumballSeeds/gumballGame.js
@@ -1,3 +1,10 @@
+var character1X = 15;
+var character2X = 185;
+var character3X = 350;
+var character4X = 515;
+var character5X = 680;
+var character6X = 845;
+var characterY = 10;
 
 $(document).ready(
     function()
@@ -7,30 +14,64 @@ $(document).ready(
     var width = $("#canvas").width();
     var height = $("#canvas").height();
     
+    var imageCounter = 0;
+    
     //Background Image Stuff
     var backImage = new Image();
     backImage.src = 'gumballSeedsBackgroundSmall.PNG';
-    backImage.onload = function(){
-       context.drawImage(backImage,0,0, width, height);
-   };
+    var characterImage = new Image();
+    characterImage.src = 'LastFinalSprite.png';
     
-    function drawPattern() 
+    backImage.onload = function()
     {
-        context.fillStyle = context.createPattern(backImage, "repeat");
-        context.fillRect(0, 0, width, height);
+       context.drawImage(backImage,0,0, width, height);
+        //where in the picture to draw, how much to draw, where to place it, how big to blow it up
+       context.drawImage(characterImage,character1X,characterY,140,130,200,340,100,100);
+    };
+    
+    function DrawBackground() 
+    {
+        context.drawImage(backImage,0,0, width, height);
     }
   
     function Initialize()
     {
-        drawPattern();
+        DrawBackground();
+        CharacterAnimation();
+        
+        if(typeof game_loop != 'undefined') clearInterval(game_loop);
+		game_loop = setInterval(UpdateMap, 120);
     }
     
-    function DrawMap()
+    function CharacterAnimation()
     {
-        context.fillStyle = "#8CFFF4";
-        context.fillRect(0,0,width,height);
-        context.strokeStyle = "white";
-	    context.strokeRect(0,0,width,height);
+        switch (imageCounter) {
+            case 1: context.drawImage(characterImage,character1X,characterY,140,130,200,340,100,100);
+                break;
+            case 2: context.drawImage(characterImage,character2X,characterY,140,130,200,340,100,100);
+                break;
+            case 3: context.drawImage(characterImage,character3X,characterY,140,130,200,340,100,100);
+                break;
+            case 4: context.drawImage(characterImage,character4X,characterY,140,130,200,340,100,100);
+                break;
+            case 5: context.drawImage(characterImage,character5X,characterY,140,130,200,340,100,100);
+                break;
+            case 6: context.drawImage(characterImage,character6X,characterY,140,130,200,340,100,100);
+                break;
+            default:
+                imageCounter = 2;
+                context.drawImage(characterImage,character1X,characterY,140,130,200,340,100,100);
+                break;
+        }
+        
+        imageCounter++;
     }
-    //Initialize();
+    
+    function UpdateMap()
+    {
+        DrawBackground();
+        CharacterAnimation();
+    }
+    
+    Initialize();
 });
